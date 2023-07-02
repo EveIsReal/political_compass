83f83332b8a66cb49fb1d3438a1505e50ba89f81
Iven T <51271307+Iv221@users.noreply.github.com>
2023-07-02 13:38:52 +0200

    Added root button on end-page

--- src/routes/end/+page.svelte
+++ src/routes/end/+page.svelte
@@ -7,9 +7,27 @@
 
 <h1>End</h1>
 <p>Dein Score: {sumOfDigits(data.score)}</p>
+<a href="/">
+    Zum Anfang
+</a>
 
 <style>
     h1 {
         color: white;
+    }
+
+    a {
+        background-color: rgb(41, 128, 179);
+		padding-top: 10px;
+		padding-bottom: 10px;
+		padding-left: 25px;
+		padding-right: 25px;
+		text-decoration: none;
+		color: white;
+		font-size: 12pt;
+		border-radius: 10px;
+		border: none;
+		cursor: pointer;
+        margin-top: 25px;
     }
 </style>
