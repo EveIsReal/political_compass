d154ab022ea043165794474b78102a9c50bff102
BuildTools <unconfigured@null.spigotmc.org>
2023-07-03 10:29:12 +0200

    added grüne-party

--- src/routes/[q_id]/+page.svelte
+++ src/routes/[q_id]/+page.svelte
@@ -1,140 +1,143 @@
 <script lang="ts">
-    import questions from "../../questions.json";
-	import { score, usedQuestions } from "../../stores";
-	import { sumOfDigits } from "../../utils";
-	import type { PageData } from "./$types";
-    export let data: PageData;
+	import questions from '../../questions.json';
+	import { score, usedQuestions } from '../../stores';
+	import { sumOfDigits } from '../../utils';
+	import type { PageData } from './$types';
+	export let data: PageData;
 
-    const question = questions[data.id].question;
-    const choices = questions[data.id].choices;
+	const question = questions[data.id].question;
+	const choices = questions[data.id].choices;
 
-    let answer = -1;
-    let isSelected: boolean; 
+	let answer = -1;
+	let isSelected: boolean;
 
-    $: isSelected = answer != -1;
+	$: isSelected = answer != -1;
 </script>
 
 <div class="wrapper">
-    <div class="content">
-        <form method="POST" class="question">
-            <p>{sumOfDigits(data.score)}</p>
-            <p>{data.usedQuestions}</p>
-            <h1>{question}</h1>
+	<div class="content">
+		<form method="POST" class="question">
+			<p>{sumOfDigits(data.score)}</p>
+			<p>{data.usedQuestions}</p>
+			<h1>{question}</h1>
 
-            <div class="choices">
-                <label for="c0">
-                    <input type="radio" bind:group={answer} value={0} name="c0" class="choice">
-                    Linke
-                </label>
+			<div class="choices">
+				<label for="c0">
+					<input type="radio" bind:group={answer} value={0} name="c0" class="choice" />
+					Linke
+				</label>
 
-                <label for="c1">
-                    <input type="radio" bind:group={answer} value={1} name="c1" class="choice">
-                    SPD
-                </label>
+				<label for="c1">
+					<input type="radio" bind:group={answer} value={1} name="c1" class="choice" />
+					Grüne
+				</label>
 
-                <label for="c2">
-                    <input type="radio" bind:group={answer} value={2} name="c2" class="choice">
-                    FDP
-                </label>
+				<label for="c2">
+					<input type="radio" bind:group={answer} value={2} name="c2" class="choice" />
+					SPD
+				</label>
 
-                <label for="c3">
-                    <input type="radio" bind:group={answer} value={3} name="c3" class="choice">
-                    CDU
-                </label>
+				<label for="c3">
+					<input type="radio" bind:group={answer} value={3} name="c3" class="choice" />
+					FDP
+				</label>
 
-                <label for="c4">
-                    <input type="radio" bind:group={answer} value={4} name="c4" class="choice">
-                    AfD
-                </label>
-            </div>
-            <button type="submit" class={isSelected ? "submit-btn" : "locked"} disabled={!isSelected}>
-                Nächste
-            </button>
-            <div class="info">
-                {#if isSelected}
-                    <p>{choices[answer]}</p>
-                {/if}
-            </div>
-        </form>
-    </div>
+				<label for="c4">
+					<input type="radio" bind:group={answer} value={4} name="c4" class="choice" />
+					CDU
+				</label>
+
+				<label for="c5">
+					<input type="radio" bind:group={answer} value={5} name="c5" class="choice" />
+					AfD
+				</label>
+			</div>
+			<button type="submit" class={isSelected ? 'submit-btn' : 'locked'} disabled={!isSelected}>
+				Nächste
+			</button>
+			<div class="info">
+				{#if isSelected}
+					<p>{choices[answer]}</p>
+				{/if}
+			</div>
+		</form>
+	</div>
 </div>
 
 <style>
+	.locked {
+		cursor: not-allowed;
+		background-color: rgb(75, 152, 71);
+		padding: 15px;
+		margin-top: 25px;
+		border-radius: 10px;
+		border: none;
+		transition: all 0.1s;
+		font-size: 14pt;
+	}
 
-    .locked {
-        cursor: not-allowed;
-        background-color: rgb(75, 152, 71);
-        padding: 15px;
-        margin-top: 25px;
-        border-radius: 10px;
-        border: none;
-        transition: all .1s;
-        font-size: 14pt;
-        
-    }
+	.info {
+		width: 50%;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		margin-top: 10px;
+	}
 
-    .info {
-        width: 50%;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        margin-top: 10px;
-    }
+	.wrapper {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
 
-    .wrapper {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-    }
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		/* background-color: #242424; */
+		width: 50vw;
+		height: 100vh;
+	}
 
-    .content {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        /* background-color: #242424; */
-        width: 50vw;
-        height: 100vh;
-    }
+	.question {
+		background-color: rgb(70, 88, 230);
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		width: 100%;
+		height: 50vh;
+		border-radius: 10px;
+	}
+	.choices {
+		display: flex;
+		justify-content: center;
+		width: 100%;
+	}
 
-    .question {
-        background-color: rgb(70, 88, 230);
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        width: 100%;
-        height: 50vh;
-        border-radius: 10px;
-    }
-    .choices {
-        display: flex;
-        justify-content: center;
-        width: 100%;
-    }
+	.choice {
+		margin-left: 20px;
+	}
 
-    .choice {
-        margin-left: 20px;
-    }
+	.submit-btn {
+		padding: 15px;
+		margin-top: 25px;
+		border-radius: 10px;
+		background-color: rgb(76, 203, 69);
+		border: none;
+		transition: all 0.1s;
+		cursor: pointer;
+		font-size: 14pt;
+	}
 
-    .submit-btn {
-        padding: 15px;
-        margin-top: 25px;
-        border-radius: 10px;
-        background-color: rgb(76, 203, 69);
-        border: none;
-        transition: all .1s;
-        cursor: pointer;
-        font-size: 14pt;
-    }
+	.submit-btn:hover {
+		transition: all 0.1s;
+		-webkit-box-shadow: 1px 5px 15px 5px rgba(255, 255, 255, 0.4);
+		box-shadow: 1px 5px 15px 5px rgba(255, 255, 255, 0.4);
+	}
 
-    .submit-btn:hover {
-        transition: all .1s;
-        -webkit-box-shadow: 1px 5px 15px 5px rgba(255,255,255,0.4); 
-        box-shadow: 1px 5px 15px 5px rgba(255,255,255,0.4);
-    }
-
-    /* .choice:hover {
+	/* .choice:hover {
         transition: all .1s;
         -webkit-box-shadow: 1px 5px 15px 5px rgba(255,255,255,0.4); 
         box-shadow: 1px 5px 15px 5px rgba(255,255,255,0.4);
