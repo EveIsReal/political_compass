fb25833cfc432e15331520dbb3ae8006d2c5b55a
Iven T <51271307+Iv221@users.noreply.github.com>
2023-07-04 22:06:08 +0200

    added footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,36 @@
     import "../App.css";
 </script>
 
-<slot/>
+<div class="body">
+    <slot/>
+
+    <footer class="footer">
+        <a href="https://github.com/ThoIven/political_compass">Für Interessierte</a>
+    </footer>
+</div>
+
+<style>
+
+    .body {
+        width: 100vw;
+        height: 100vh;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-evenly;
+    }
+
+    .footer {
+        height: 100%;
+        display: flex;
+        justify-content: start;
+        align-items: end;
+        padding: 30px;
+        color: white;
+    }
+
+    .footer > a {
+        text-decoration: none;
+        color: rgb(96, 146, 238);
+    }
+
+</style>
